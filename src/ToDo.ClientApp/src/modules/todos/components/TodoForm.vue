59519a4fbf2c3a596b226f6f6edbb791d5d94ad8
Xolmuratov02 <xolmuratovqurbonali@gmail.com>
2023-12-27 19:25:21 +0500

    Add calendar

--- src/ToDo.ClientApp/src/modules/todos/components/TodoForm.vue
+++ src/ToDo.ClientApp/src/modules/todos/components/TodoForm.vue
@@ -1,32 +1,48 @@
 <template>
 
   <div class="h-20 gap-x-4 w-full border border-gray-500 rounded-xl p-4 flex items-center">
+    <form @submit.prevent="submitAsync" class="w-full flex relative items-center justify-center">
 
-
-    <v-form @submit.prevent="submitAsync" class="w-full flex">
+      <!-- Title input -->
       <div class="flex-grow">
         <input type="text" placeholder="Add task" v-model="todo.title"
-               class="input text-lg focus:outline-none focus:border-sky-500 contrast-less:border-slate-300 text-slate-300">
+               class="input text-lg">
       </div>
+
       <div class="flex gap-x-4">
 
         <!-- Due time picker -->
-        <button class="text-2xl flex items-center justify-center relative" @click="toggleDueTimePicker">
+        <date-time-picker class="z-20 absolute bottom-0 right-0"
+                          v-if="showDueTimePicker"
+                          @onClose="toggleDueTimePicker"
+                          :min-date="minDate"
+                          v-model="todo.dueTime"/>
+        <button ref="dueTimeButton" type="button" class="text-2xl flex items-center justify-center relative"
+                @click="toggleDueTimePicker">
           <i class="fa-regular btn-hover fa-calendar theme-icon mr-1"/>
-          <v-date-picker v-if="showDueTimePicker" v-model="todo.dueTime" class="z-20 absolute"/>
         </button>
 
         <!-- Reminder time picker -->
-        <button class="text-2xl flex items-center justify-center relative" @click="toggleReminderTimePicker">
+        <date-time-picker class="z-20 absolute bottom-0 right-0"
+                          v-if="showReminderTimePicker"
+                          @onClose="toggleReminderTimePicker"
+                          :min-date="minDate"
+                          :max-date="todo.dueTime"
+                          v-model="todo.reminderTime"/>
+        <button type="button" class="text-2xl flex items-center justify-center relative"
+                @click="toggleReminderTimePicker">
           <i class="fa-regular btn-hover fa-bell theme-icon mr-1"></i>
-          <v-date-picker v-if="showReminderTimePicker" v-model="todo.reminderTime" class="z-20 absolute"/>
         </button>
 
-        <button type="submit">Submit</button>
+        <!-- Submit button -->
+        <button type="submit"
+                class="w-7 h-7 btn-hover rounded-full bg-textSecondaryColor flex items-center justify-center">
+          <i class="fa-solid fa-check"></i>
+        </button>
 
       </div>
 
-    </v-form>
+    </form>
   </div>
 
 </template>
@@ -37,9 +53,8 @@ import { ref, watch } from "vue";
 import { Utils } from "@/infrastructure/extensions/ObjectExtensions";
 import { ToDoItem } from "@/modules/todos/models/ToDoItem";
 import { TodoApiClient } from "@/infrastructure/apiClients/todoApiClient/brokers/TodoApiClient";
-import { useDate } from "vuetify";
+import DateTimePicker from "@/common/components/DateTimePicker.vue";
 
-const adapter = useDate();
 const todoApiClient = new TodoApiClient();
 
 const emit = defineEmits<{
@@ -54,10 +69,20 @@ const props = defineProps({
   }
 });
 
-const isEditing = ref<boolean>(false);
+const minDate = new Date();
 const todo = ref<ToDoItem>(new ToDoItem());
+const isEditing = ref<boolean>(false);
 const showDueTimePicker = ref<boolean>(false);
 const showReminderTimePicker = ref<boolean>(false);
+
+watch(() => props.editTodo, () => {
+  if (props.editTodo) {
+    isEditing.value = true;
+    todo.value = Utils.deepClone(props.editTodo);
+    todo.value.dueTime = new Date(props.editTodo.dueTime);
+    todo.value.reminderTime = new Date(props.editTodo.reminderTime);
+  }
+});
 
 const submitAsync = async () => {
   let result: boolean;
@@ -73,28 +98,19 @@ const submitAsync = async () => {
     resetForm();
 }
 
-const resetForm = () => {
-  todo.value = new ToDoItem();
-}
+const resetForm = () => todo.value = new ToDoItem();
 
 const createTodoAsync = async () => {
-  console.log(todo.value);
-
   const response = await todoApiClient.todos.createAsync(todo.value);
 
-  if (response.isSuccess)
-    emit("addNewTodo", response.response!)
+  if (response.isSuccess) emit("addNewTodo", response.response!)
 
   return response.isSuccess;
 }
 
 const updateTodoAsync = async () => {
   const response = await todoApiClient.todos.updateAsync(todo.value);
-
-  if (response.isSuccess)
-  {
-    Object.assign(props.editTodo!, todo.value!);
-  }
+  if (response.isSuccess) Object.assign(props.editTodo!, todo.value!);
 
   return response.isSuccess;
 }
@@ -108,14 +124,5 @@ const toggleReminderTimePicker = () => {
   showReminderTimePicker.value = !showReminderTimePicker.value;
   showDueTimePicker.value = false;
 }
-
-watch(() => props.editTodo, () => {
-  if (props.editTodo) {
-    isEditing.value = true;
-    todo.value = Utils.deepClone(props.editTodo);
-    todo.value.dueTime = new Date(props.editTodo.dueTime);
-    todo.value.reminderTime = new Date(props.editTodo.reminderTime);
-  }
-});
 
 </script>
